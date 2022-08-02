db06452b2811842ab472aafa8515fa5818542484
DerFetzer <kontakt@der-fetzer.de>
2022-08-02 19:35:03 +0200

    which-key bindings, lua lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,10 +1,13 @@
+local wk = require("which-key")
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+wk.register({
+    ['<leader>e'] = { vim.diagnostic.open_float, "Diagnostics open" },
+    ['[d'] = { vim.diagnostic.goto_prev, "Diagnostics previous" },
+    [']d'] = { vim.diagnostic.goto_next, "Diagnostics next" },
+    ['<leader>q'] = { vim.diagnostic.setloclist, "Diagnostics locations" },
+})
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -16,22 +19,21 @@ local on_attach = function(client, bufnr)
 
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap=true, silent=true, buffer=bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set('n', '<leader>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, bufopts)
+    wk.register({
+        ['gD'] = { vim.lsp.buf.declaration, "Go to declaration" },
+        ['gd'] = { vim.lsp.buf.definition, "Go to definition" },
+        ['K'] = { vim.lsp.buf.hover, "Hover" },
+        ['gi'] = { vim.lsp.buf.implementation, "Go to implementation" },
+        ['<C-k>'] = { vim.lsp.buf.signature_help, "Show signature" },
+        ['<leader>wa'] = { vim.lsp.buf.add_workspace_folder, "Add workspace folder" },
+        ['<leader>wr'] = { vim.lsp.buf.remove_workspace_folder, "Remove workspace following" },
+        ['<leader>wl'] = { function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, "List workspace foders" },
+        ['<leader>D'] = { vim.lsp.buf.type_definition, "Go to type definition" },
+        ['<leader>rn'] = { vim.lsp.buf.rename, "Rename" },
+        ['<leader>ca'] = { vim.lsp.buf.code_action, "Code action" },
+        ['gr'] = { vim.lsp.buf.references, "Show references" },
+        ['<leader>f'] = { vim.lsp.buf.formatting, "Format" },
+    })
 end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -55,3 +57,30 @@ require'lspconfig'.pyright.setup{
     on_attach = on_attach,
 	capabilities = capabilities,
 }
+
+require'lspconfig'.sumneko_lua.setup {
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+   },
+    on_attach = on_attach,
+    capabilities = capabilities,
+
+}
+
