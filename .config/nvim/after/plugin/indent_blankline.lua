aa73a6173f070e273d41aee032dd380d31f4d36d
DerFetzer <kontakt@der-fetzer.de>
2022-08-03 18:41:55 +0200

    Context for indent_blankline only for linux

--- .config/nvim/after/plugin/indent_blankline.lua
+++ .config/nvim/after/plugin/indent_blankline.lua
@@ -1,7 +1,13 @@
+local context
+if vim.fn.has("win32") == 1 then
+    context = false
+else
+    context = true
+end
+
 require("indent_blankline").setup {
     -- for example, context is off by default, use this to turn it on
     -- Crashes on Windows
-    
-    -- show_current_context = true,
-    -- show_current_context_start = true,
+    show_current_context = context,
+    show_current_context_start = context,
 }
