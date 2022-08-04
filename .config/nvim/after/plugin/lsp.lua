f2d06a6d3e5cf6e1cc4f3ed4e83c128876844157
DerFetzer <kontakt@der-fetzer.de>
2022-08-04 12:51:28 +0200

    clangd

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -78,3 +78,8 @@ require 'lspconfig'.sumneko_lua.setup {
     capabilities = capabilities,
 
 }
+
+require 'lspconfig'.clangd.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
