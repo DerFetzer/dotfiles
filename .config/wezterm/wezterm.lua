586b7220edb9b6c304f7b51ab40e36b6ee93d1aa
DerFetzer <kontakt@der-fetzer.de>
2023-12-15 22:28:28 +0100

    Use regular font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -40,7 +40,7 @@ config.scrollback_lines = 1000000
 -- Appearance
 config.window_decorations = "RESIZE"
 
-config.font = wezterm.font('JetBrains Mono NF', { weight = 'Regular' })
+config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
 config.font_size = 9.5
 
 config.color_scheme = 'Catppuccin Macchiato'
