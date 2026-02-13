5db24cefb3c4b203d74f3c85af0dc74191dbd5eb
DerFetzer <kontakt@der-fetzer.de>
2026-02-13 14:32:37 +0100

    Use treesitter only in specific files

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,7 +1,22 @@
-require('nvim-treesitter').install({ "c", "lua", "python", "rust", "cpp", "nu" })
+local langs = { "c", "lua", "python", "rust", "cpp", "nu" }
 
-vim.wo.foldmethod = "expr"
-vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.wo.foldenable = false
+require('nvim-treesitter').install(langs)
 
 require 'treesitter-context'.setup({})
+
+-- Autocommands
+local ts_group = vim.api.nvim_create_augroup("TS", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = (langs),
+  callback = function()
+      -- syntax highlighting, provided by Neovim
+      vim.treesitter.start()
+      -- folds, provided by Neovim
+      vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+      vim.wo.foldmethod = 'expr'
+      vim.wo.foldlevel = 99
+      -- indentation, provided by nvim-treesitter
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+  group = ts_group
+})
